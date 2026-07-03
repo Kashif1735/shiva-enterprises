de0868b0d64d31876fe931588a76a984319a5429
Kashif1735 <kashiffaiz173@gmail.com>
2026-07-03 09:20:40 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/shiva-enterprises/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
